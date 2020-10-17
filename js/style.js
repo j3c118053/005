e0aff03e4377e1a816eb45f50a644706f5071ba2
LAPTOP-1F1FB8RS\user <bagusrahmaddi2000@gmail.com>
2020-10-17 15:30:15 +0700

    Commit2 -  Upload JLINE 005 with css and javascript

---
+++ js/style.js
@@ -0,0 +1,15 @@
+
+const scrollToTop = document.querySelector("#scrollToTop");
+
+scrollToTop.addEventListener("click",function(){
+    //window.scrollTo(0,0);
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth"
+
+    });
+
+    
+});
+
